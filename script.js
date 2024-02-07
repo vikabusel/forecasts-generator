a0510469731fa2bb19454f4ec9e751e6d0ee8788
vikabusel <vbusel@inbox.ru>
2024-02-07 22:10:37 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,44 @@
 /* Генерация предсказания должна происходить при клике на кнопку «предсказать судьбу» */
+const button = document.querySelector('.forecast-btn');
+const forecasts = document.querySelector ('.forecasts');
+const forecastItems = document.querySelector('#forecast-item');
+const probability = document.querySelector('.current-forecast p');
+const prediction = document.querySelector ('.current-forecast h1');
+let predictionText = '';
+
+function getRandomNumber(min, max) {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+button.addEventListener('click', function() {
+    const predictionNumber = getRandomNumber(1, 6);
+    const randomProbability = getRandomNumber(0, 100);
+
+    if (predictionText) {
+    const items = forecastItems.content.cloneNode(true);
+    const randomPrediction = items.querySelector('p');
+    randomPrediction.textContent = predictionText;
+    forecasts.append(items);
+    }
+
+    if (predictionNumber == 1) {
+        predictionText = "Пришло время действовать!";
+      } else if (predictionNumber == 2) {
+        predictionText = "Что ни делается - все к лучшему";
+      } else if (predictionNumber == 3) {
+        predictionText = "Делай что пожелаешь и не жалей!"; 
+      } else if (predictionNumber == 4) {
+        predictionText = "Ты уедешь в путешествие очень скоро";
+      } else {
+        predictionText = "Вскоре к вам явится кто-то неожиданный";
+      }
+   
+    prediction.textContent = predictionText;
+    probability.textContent = `Вероятность: ${randomProbability}%`;
+});
 
 /* Заранее заготовь 3-5 предсказаний и в зависимости от того, как лягут карты судьбы (или что скажет Math.random) показывай их пользователю */
+
 
 /* Подставляй текст нового предсказания в .current-forecast h1 */
 
